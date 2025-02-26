618ff5b2ae193edd76c2b9f483c03ea28bf91f9e
DESKTOP-MQT3RDO\ISE <mitomikan7@gmail.com>
2025-02-27 02:30:40 +0900

    電通大 解答速報追加

---
+++ note/notes/config.js
@@ -0,0 +1,14 @@
+window.MathJax={
+    tex:{
+        tags:"ams",
+        macros:{
+            x:"{\\times}",
+            bm:["{\\boldsymbol{#1}}",1],
+            dd:["{\\frac{\\partial#1}{\\partial#2}}",2],
+            N:"{\\mathbb{N}}"
+        }
+    },
+    chtml:{
+        mtextInheritFont:true
+    }
+};
